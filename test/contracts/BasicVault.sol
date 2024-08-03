e1d7bb93d9d0bfd269611457e8e42f877f19d7a1
Mujahid002 <mujahidshaik2002@gmail.com>
2024-08-04 00:00:07 +0530

    initial commit

---
+++ test/contracts/BasicVault.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Vault {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Deposit(uint amount, uint when);
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+    function deposit() payable public {
+        emit Deposit(msg.value, block.timestamp);
+
+    }
+
+    function withdraw() public {
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "you aren't the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
+    }
+}
