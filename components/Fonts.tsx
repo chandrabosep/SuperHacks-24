9fc1fa908c1427529f54b71b4666d790aae24ef8
Chandra Bose <chandrabosep3112@gmail.com>
2024-08-03 20:23:42 +0530

    feat:fonts

---
+++ components/Fonts.tsx
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const gtHead = localFont({
+	src: "../public/fonts/Extended-Regular-Italic.otf",
+});
+
+export const gtSideHead = localFont({
+	src: "../public/fonts/standard-Regular-Italic.otf",
+});
+
+export const gtText = localFont({
+	src: "../public/fonts/Extended-Regular.otf",
+});
